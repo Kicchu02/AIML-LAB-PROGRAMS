3bdc70858664578362b2e23cb08b15c6d2e58c52
Kicchu02 <kicchu.2002@gmail.com>
2024-01-07 17:51:27 +0530

    added labels

--- Kmeans_and_EM.py
+++ Kmeans_and_EM.py
@@ -9,24 +9,35 @@ dataset = pd.read_csv('iris.csv', names=['slength','swidth','plength','pwidth','
 # prepare the data
 X = dataset.iloc[:,:-1].values
 
-colormap = {'Iris-setosa': 'red', 'Iris-versicolor': 'green', 'Iris-virginica': 'blue'}
+# set the color map for each species (dictionary)
+colormap = {
+  'Iris-setosa': 'red', 
+  'Iris-versicolor': 'green', 
+  'Iris-virginica': 'blue'
+}
 
-# perform both clustering algorithms
+# perform K Means clustering and get labels
 kmeans = KMeans(n_clusters=3)
 kmeans_labels = kmeans.fit_predict(X)
 
+# perform EM algorithm and get labels
 em = GaussianMixture(n_components=3)
 em_labels = em.fit_predict(X)
 
+# plot the original
 plt.subplot(1,3,1)
 plt.scatter(X[:, 2], X[:,3], c=dataset['species'].map(colormap))
+plt.title('Original')
 
+# plot the kmeans
 plt.subplot(1,3,2)
 plt.scatter(X[:,2], X[:,3], c=kmeans_labels)
 plt.title('K Means Clustering')
 
+# plot the em
 plt.subplot(1,3,3)
 plt.scatter(X[:,2], X[:,3], c=em_labels)
 plt.title('EM Clustering')
 
+# show all the three graphs
 plt.show()
